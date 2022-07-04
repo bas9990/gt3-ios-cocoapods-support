d953d7363728dcce981340aac3d26152e5db44d4
bas9990 <39779834+bas9990@users.noreply.github.com>
2022-07-04 14:49:33 +0200

    create new file

---
+++ Package.swift
@@ -0,0 +1,16 @@
+let package = Package(
+    name: "GT3Captcha",
+    products: [
+        .library(
+            name: "GT3Captcha",
+            targets: ["GT3Captcha"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "GT3Captcha",
+            path: "Sources"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
